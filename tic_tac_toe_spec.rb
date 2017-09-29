4e3244a9c73b97eda291121c0bd477fd8cb005f0
Tasmin Steer <tasminsteer@hotmail.com>
2017-09-29 15:49:02 +0100

    refactor specs

--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -40,60 +40,62 @@ RSpec.describe do
         end
       end
 
-      it 'a player can place a piece on the board' do
-        game_board = ['x','','','','','','','','']
+      context 'with valid positions' do
+        it 'a player can place a piece on the board' do
+          game_board = ['x','','','','','','','','']
 
-        current_game.place(0,'x')
+          current_game.place(0,'x')
+          
+          expect(current_game.board).to eq(game_board)
+        end
+
+        it 'a player can place a piece on the board' do
+          game_board = ['','x','','','','','','','']
+          current_game.place(1,'x')
+
+          expect(current_game.board).to eq(game_board)
+        end
+
+        it 'a player can place multiple pieces on the board' do
+          game_board = ['x','','x','','x','','','','']
         
-        expect(current_game.board).to eq(game_board)
-      end
-
-      it 'a player can place a piece on the board' do
-        game_board = ['','x','','','','','','','']
-        current_game.place(1,'x')
-
-        expect(current_game.board).to eq(game_board)
-      end
-
-      it 'a player can place multiple pieces on the board' do
-        game_board = ['x','','x','','x','','','','']
-      
-        current_game.place(0, 'x')
-        current_game.place(2, 'x')
-        current_game.place(4, 'x')
-
-        expect(current_game.board).to eq(game_board)
-      end
-
-      context 'and a player has won' do
-        it 'recognises a win' do
           current_game.place(0, 'x')
-          current_game.place(3, 'x')
-
-          expect(current_game.place(6, 'x')).to eq("Player x has won")
-        end
-
-        it 'recognises a win' do
-          current_game.place(0, 'x')
-          current_game.place(1, 'x')
-
-          expect(current_game.place(2, 'x')).to eq("Player x has won")
-        end
-
-        it 'recognises a win' do
-          current_game.place(0, 'x')
+          current_game.place(2, 'x')
           current_game.place(4, 'x')
 
-          expect(current_game.place(8, 'x')).to eq("Player x has won")
+          expect(current_game.board).to eq(game_board)
         end
-      end
 
-      context 'and a player has not won' do
-        it 'recognises a player has not won' do
-          current_game.place(0, 'x')
-          current_game.place(3, 'x')
+        context 'and a player has won' do
+          it 'recognises a win' do
+            current_game.place(0, 'x')
+            current_game.place(3, 'x')
 
-          expect(current_game.place(8, 'x')).to_not eq("Player x has won")
+            expect(current_game.place(6, 'x')).to eq("Player x has won")
+          end
+
+          it 'recognises a win' do
+            current_game.place(0, 'x')
+            current_game.place(1, 'x')
+
+            expect(current_game.place(2, 'x')).to eq("Player x has won")
+          end
+
+          it 'recognises a win' do
+            current_game.place(0, 'x')
+            current_game.place(4, 'x')
+
+            expect(current_game.place(8, 'x')).to eq("Player x has won")
+          end
+        end
+
+        context 'and a player has not won' do
+          it 'recognises a player has not won' do
+            current_game.place(0, 'x')
+            current_game.place(3, 'x')
+
+            expect(current_game.place(8, 'x')).to_not eq("Player x has won")
+          end
         end
       end
     end
